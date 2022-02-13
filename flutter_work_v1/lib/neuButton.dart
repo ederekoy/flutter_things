e474abae95acd3f78b549dc55682b0328cd34aec
Emre Can Dereköy <emrecanderekoy@gmail.com>
2022-02-13 13:32:29 +0300

    Dart package update

--- flutter_work_v1/lib/neuButton.dart
+++ flutter_work_v1/lib/neuButton.dart
@@ -1,2 +1,47 @@
 import 'package:flutter/material.dart';
 
+class NeuButton extends StatefulWidget {
+  const NeuButton({Key? key, required this.isElevated}) : super(key: key);
+
+  final bool isElevated;
+
+  @override
+  State<NeuButton> createState() => _NeuButtonState(isElevated);
+}
+
+class _NeuButtonState extends State<NeuButton> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        height: 200,
+        width: 200,
+        decoration: BoxDecoration(
+            color: Colors.grey[300],
+            borderRadius: BorderRadius.circular(50),
+            boxShadow: isElevated
+                ? [
+                    BoxShadow(
+                      color: Colors.grey[500]!,
+                      offset: const Offset(4, 4),
+                      blurRadius: 15,
+                      spreadRadius: 1,
+                    ),
+                    const BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(-4, -4),
+                      blurRadius: 15,
+                      spreadRadius: 1,
+                    )
+                  ]
+                : null),
+      ),
+      onTap: (() {
+        setState(() {
+          isElevated = !isElevated;
+        });
+      }),
+    );
+  }
+}
